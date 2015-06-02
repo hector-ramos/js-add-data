68572d1b0608dd37d72ee56aee4950a8072c8698
Hector Ramos <hector.ramos@ninthdesign.com>
2015-06-02 13:28:43 +0800

    modified script.js(store data in array)

---
+++ script.js
@@ -0,0 +1,15 @@
+var person = [
+    {
+        name: 'Annie',
+        tel: '671-787-7262',
+        ammount: 20.01
+    }, {
+        name: 'Amie',
+        tel: '671-787-7262',
+        ammount: 20.01
+    }, {
+        name: 'Angelo',
+        tel: '671-787-7262',
+        ammount: 20.01
+    }
+];
